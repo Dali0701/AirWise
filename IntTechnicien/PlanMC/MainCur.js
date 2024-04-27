2ceb225d7d6264df1c82bd17d17fcc68a3d6f6a4
Dali0701 <dalichaabani@gmail.com>
2024-04-27 14:57:15 +0200

    first commit

---
+++ IntTechnicien/PlanMC/MainCur.js
@@ -0,0 +1,14 @@
+const openPageCR = document.getElementById("CR");
+
+// Add click event listener
+openPageCR.addEventListener("click", function() {
+  window.location.href ="../CR/CompteRendu.HTML";
+});
+
+const openPageMP = document.getElementById("MP");
+
+// Add click event listener
+openPageMP.addEventListener("click", function() {
+
+  window.location.href ="../PlanMP/MainPrev.HTML";
+});
---
+++ IntTechnicien/PlanMP/MainPrev.js
@@ -0,0 +1,14 @@
+const openPageCR = document.getElementById("CR");
+
+// Add click event listener
+openPageCR.addEventListener("click", function() {
+  window.location.href ="../CR/CompteRendu.HTML";
+});
+
+const openPageMC = document.getElementById("MC");
+
+// Add click event listener
+openPageMC.addEventListener("click", function() {
+
+  window.location.href ="../PlanMC/MainCur.HTML";
+});
